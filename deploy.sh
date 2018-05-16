87d6f180aa8d94a9308b480710ccb261b05baa83
Seraph_JACK <seraphjack@outlook.com>
2018-05-16 19:53:23 +0800

    Update

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+gitbook build
+rm -rf /s /q ../seraphjack.coding.me/chem
+rm -rf /s /q ../seraphjack.coding.me/math
+rm -rf /s /q ../seraphjack.coding.me/gitbook
+rm ../seraphjack.coding.me/index.html
+rm ../seraphjack.coding.me/search_index.json
+rm ../seraphjack.coding.me/package-lock.json
+git add .
+git commit -m"Update"
+git push
+
+cp ./_book/* ../seraphjack.coding.me/ -r
