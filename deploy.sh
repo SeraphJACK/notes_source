abfe030f2e8f7204efec1b23aad629fb79f6e982
Seraph_JACK <seraphjack@outlook.com>
2018-09-09 12:46:34 +0800

    Update

--- deploy.sh
+++ deploy.sh
@@ -1,31 +1,28 @@
 #!/bin/sh
 gitbook build
 
-echo Press any key to continue...
-read -n 1
-rm -rf /s /q ../seraphjack.coding.me/chem
-rm -rf /s /q ../seraphjack.coding.me/math
-rm -rf /s /q ../seraphjack.coding.me/gitbook
-rm ../seraphjack.coding.me/index.html
-rm ../seraphjack.coding.me/search_index.json
-rm ../seraphjack.coding.me/package-lock.json
+#echo Press any key to continue...
+#read -n 1
+rm -rf /s /q ../seraphjack.coding.me/chem>/dev/null
+rm -rf /s /q ../seraphjack.coding.me/math>/dev/null
+rm -rf /s /q ../seraphjack.coding.me/gitbook>/dev/null
+rm ../seraphjack.coding.me/index.html>/dev/null
+rm ../seraphjack.coding.me/search_index.json>/dev/null
+rm ../seraphjack.coding.me/package-lock.json>/dev/null
 git pull
 git add .
-git commit -m"Update"
+git commit -m"Update">/dev/null
 git push
 
-echo Press any key to continue...
-read -n 1
+#echo Press any key to continue...
+#read -n 1
 
-rm ./_book/deploy.sh
-rm ./_book/bash.exe.stackdump
+rm ./_book/deploy.sh>/dev/null
+rm ./_book/bash.exe.stackdump>/dev/null
 cp ./_book/* ../seraphjack.coding.me/ -r
 
 cd ../seraphjack.coding.me/
 git pull
 git add .
-git commit -m"Site Update"
+git commit -m"Site Update">/dev/null
 git push
-
-echo Press any key to continue...
-read -n 1
