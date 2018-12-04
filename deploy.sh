9f50cb53cb33bec61a41475e2d4492d370456559
SeraphJACK <seraphjack@outlook.com>
2018-12-04 13:00:52 +0800

    Update

--- deploy.sh
+++ deploy.sh
@@ -1,28 +1,27 @@
 #!/bin/sh
 gitbook build
 
-#echo Press any key to continue...
-#read -n 1
-rm -rf /s /q ../seraphjack.coding.me/chem>/dev/null
-rm -rf /s /q ../seraphjack.coding.me/math>/dev/null
-rm -rf /s /q ../seraphjack.coding.me/gitbook>/dev/null
-rm ../seraphjack.coding.me/index.html>/dev/null
-rm ../seraphjack.coding.me/search_index.json>/dev/null
-rm ../seraphjack.coding.me/package-lock.json>/dev/null
-git pull
-git add .
-git commit -m"Update">/dev/null
-git push
+if [ $? -eq 0 ]; then
+    rm -rf /s /q ../seraphjack.coding.me/chem>/dev/null 2>&1
+    rm -rf /s /q ../seraphjack.coding.me/math>/dev/null 2>&1
+    rm -rf /s /q ../seraphjack.coding.me/gitbook>/dev/null 2>&1
+    rm ../seraphjack.coding.me/index.html>/dev/null 2>&1
+    rm ../seraphjack.coding.me/search_index.json>/dev/null 2>&1
+    rm ../seraphjack.coding.me/package-lock.json>/dev/null 2>&1
+    git pull
+    git add .
+    git commit -m"Update">/dev/null
+    git push
 
-#echo Press any key to continue...
-#read -n 1
+    rm ./_book/deploy.sh>/dev/null
+    cp ./_book/* ../seraphjack.coding.me/ -r
 
-rm ./_book/deploy.sh>/dev/null
-#rm ./_book/bash.exe.stackdump>/dev/null
-cp ./_book/* ../seraphjack.coding.me/ -r
-
-cd ../seraphjack.coding.me/
-git pull
-git add .
-git commit -m"Site Update">/dev/null
-git push
+    cd ../seraphjack.coding.me/
+    git pull
+    git add .
+    git commit -m"Site Update">/dev/null
+    git push
+else
+    echo "Gitbook build failed"
+    exit 1
+fi
